609889d3a489ac6dfae6db9afcc323d6fc67b10e
danielderma <danielderma.contact@gmail.com>
2023-03-11 22:12:27 -0600

    add: firebase-admin

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,9 +5,11 @@ import { parse } from 'rss-to-json'
 import { useAudioPlayer } from '@/components/AudioProvider'
 import { Container } from '@/components/Container'
 import { PlayButton } from '@/components/player/PlayButton'
+import Edit from '@/icons/Edit'
+import { db } from '@/src/services/firebase'
 
 export default function EpisodeEntry({ episode }) {
-  let date = new Date(episode.published)
+  let date = new Date()
 
   let audioPlayerData = useMemo(
     () => ({
@@ -34,14 +36,17 @@ export default function EpisodeEntry({ episode }) {
             <div className="flex items-center gap-6">
               <PlayButton player={player} size="large" />
               <div className="flex flex-col">
-                <h1 className="mt-2 text-4xl font-bold text-slate-900">
-                  {episode.title}
-                </h1>
+                <div className="group relative">
+                  <h1 className="mt-2  text-4xl font-bold text-slate-900">
+                    {episode.title}
+                  </h1>
+                  <Edit className="absolute -right-4 -top-4 hidden h-6 w-6 cursor-pointer group-hover:block" />
+                </div>
                 <time
                   dateTime={date.toISOString()}
                   className="-order-1 font-mono text-sm leading-7 text-slate-500"
                 >
-                  {new Intl.DateTimeFormat('en-US', {
+                  {new Intl.DateTimeFormat('es-ES', {
                     year: 'numeric',
                     month: 'long',
                     day: 'numeric',
@@ -49,15 +54,47 @@ export default function EpisodeEntry({ episode }) {
                 </time>
               </div>
             </div>
-            <p className="ml-24 mt-3 text-lg font-medium leading-8 text-slate-700">
-              {episode.description}
-            </p>
+            <div className="group relative">
+              <p className="ml-24 mt-3 text-lg font-medium leading-8 text-slate-700">
+                {episode.description}
+              </p>
+              <Edit className="absolute -right-4 -top-4 hidden h-6 w-6 cursor-pointer group-hover:block" />
+            </div>
           </header>
           <hr className="my-12 border-gray-200" />
-          <div
+          {/* <div
             className="prose prose-slate mt-14 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-900 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2:nth-of-type(3n)]:before:bg-violet-200"
             dangerouslySetInnerHTML={{ __html: episode.content }}
-          />
+          /> */}
+          <div className="prose prose-slate mt-14 [&>div>h2]:mt-12 [&>div>h2]:flex [&>div>h2]:items-center [&>div>h2]:font-mono [&>div>h2]:text-sm [&>div>h2]:font-medium [&>div>h2]:leading-7 [&>div>h2]:text-slate-900 [&>div>h2]:before:mr-3 [&>div>h2]:before:h-3 [&>div>h2]:before:w-1.5 [&>div>h2]:before:rounded-r-full [&>div>h2]:before:bg-cyan-200 [&>div>ul]:mt-6 [&>div>ul]:list-['\2013\20'] [&>div>ul]:pl-5 [&>div>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>div>h2:nth-of-type(3n)]:before:bg-violet-200">
+            <div className="group relative w-max">
+              <h2 id="topics" className="">
+                Topicsasdf
+              </h2>
+              <Edit className="absolute -right-4 -top-4 hidden h-6 w-6 cursor-pointer group-hover:block " />
+            </div>
+            <div className="group relative">
+              <ul>
+                <li>
+                  What are TPS reports exactly, and what’s the motivation for
+                  adding the cover sheet?
+                </li>
+                <li>
+                  How Bill preserves such a cool and calm demeanor, despite the
+                  extreme consequences Initech faces for not finishing their Y2K
+                  upgrades on time, and why it’s important to protect the staff
+                  from that stress
+                </li>
+                <li>
+                  Why suspenders and belts aren’t enough on their own, and
+                  should be used together
+                </li>
+                <li>The backstory behind how Bill purchased his Porsche 911</li>
+                <li>The real reason he needed the red stapler for himself</li>
+              </ul>
+              <Edit className="absolute -right-4 -top-4 hidden h-6 w-6 cursor-pointer group-hover:block " />
+            </div>
+          </div>
         </Container>
       </article>
     </>
@@ -65,6 +102,9 @@ export default function EpisodeEntry({ episode }) {
 }
 
 export async function getServerSideProps() {
+  const data = db.collection('main').doc('home')
+  const doc = await data.get()
+  console.log(doc.data())
   let feed = await parse('https://their-side-feed.vercel.app/api/feed')
   let episode = feed.items
     .map(({ id, title, description, content, enclosures, published }) => ({
---
+++ src/src/services/firebase.js
@@ -0,0 +1,26 @@
+let admin = require('firebase-admin')
+
+let serviceAccount = {
+  type: process.env.FIREBASE_ADMIN_TYPE || '',
+  project_id: process.env.FIREBASE_ADMIN_PROJECT_ID || 'test-project',
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID || '',
+  private_key:
+    process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n') || 'REDACTED',
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL || 'REDACTED',
+  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID || '',
+  auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI || '',
+  token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI || '',
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL || '',
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL || '',
+}
+
+console.log(serviceAccount)
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  })
+}
+
+export const db = admin.firestore()
