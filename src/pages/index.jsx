d1559d66cf26716f8ecdd21adf6cc0f565f2cfd1
danielderma <danielderma.contact@gmail.com>
2023-03-13 19:12:53 -0600

    add: direct update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,8 +6,9 @@ import { Container } from '@/components/Container'
 import { PlayButton } from '@/components/player/PlayButton'
 import Edit from '@/icons/Edit'
 import { db } from '@/src/services/firebase'
-import { useSession } from 'next-auth/react'
 import { removeEmpty } from '@/utils/index'
+import { useSession } from 'next-auth/react'
+import sanitizeHtml from 'sanitize-html'
 
 export default function EpisodeEntry({ data }) {
   const [date, setDate] = useState('')
@@ -37,54 +38,32 @@ export default function EpisodeEntry({ data }) {
   )
   let player = useAudioPlayer(audioPlayerData)
 
-  const handleEditTitle = () => {
-    refTitle.current.contentEditable = true
+  const handleEdit = (ref) => {
+    ref.current.contentEditable = true
     const range = document.createRange()
-    range.selectNodeContents(refTitle.current)
+    range.selectNodeContents(ref.current)
     const selection = window.getSelection()
     selection.removeAllRanges()
     selection.addRange(range)
   }
 
-  const handleEditTitleBlur = () => {
-    refTitle.current.contentEditable = false
-    handleSave()
-  }
-
-  const handleEditDescripcion = () => {
-    refDescription.current.contentEditable = true
-    const range = document.createRange()
-    range.selectNodeContents(refDescription.current)
-    const selection = window.getSelection()
-    selection.removeAllRanges()
-    selection.addRange(range)
-  }
-
-  const handleEditDescripcionBlur = () => {
-    refDescription.current.contentEditable = false
-  }
-
-  const handleEditTopics = () => {
-    refTopiscs.current.contentEditable = true
-    const range = document.createRange()
-    range.selectNodeContents(refTopiscs.current)
-    const selection = window.getSelection()
-    selection.removeAllRanges()
-    selection.addRange(range)
-  }
-
-  const handleEditTopicsBlur = () => {
-    refTopiscs.current.contentEditable = false
+  const handlerBlur = (ref) => {
+    ref.current.contentEditable = false
     handleSave()
   }
 
   const handleSave = () => {
     setLoading(true)
+    const isTitleEmpty = refTopiscs.current.innerHTML === ''
     const newTitle = refTitle.current.textContent
     const newDescription = refDescription.current.textContent
-    const newTopics = refTopiscs.current.textContent
+    const newTopics = isTitleEmpty
+      ? '<li><br></li>'
+      : refTopiscs.current.innerHTML
 
-    console.log(newTopics)
+    if (isTitleEmpty) {
+      refTopiscs.current.innerHTML = '<li><br></li>'
+    }
 
     const body = removeEmpty({
       title: data.title === newTitle ? null : newTitle,
@@ -116,7 +95,11 @@ export default function EpisodeEntry({ data }) {
         <title>RADIO TEC HALCONES - {data.title}</title>
         <meta name="description" content={data.description} />
       </Head>
-      <article className={`py-16 lg:py-36 ${loading ? 'cursor-wait' : ''}`}>
+      <article
+        className={`py-16 lg:py-36 ${
+          loading ? 'cursor-wait' : 'cursor-default'
+        }`}
+      >
         <Container>
           <header className="flex flex-col">
             <div className="flex items-center gap-6">
@@ -126,11 +109,11 @@ export default function EpisodeEntry({ data }) {
                   <h1
                     className="mt-2  text-4xl font-bold text-slate-900"
                     ref={refTitle}
-                    onBlur={handleEditTitleBlur}
+                    onBlur={() => handlerBlur(refTitle)}
                   >
                     {data.title}
                   </h1>
-                  <button onClick={handleEditTitle}>
+                  <button onClick={() => handleEdit(refTitle)}>
                     <Edit
                       className={`absolute -right-4 -top-4 hidden h-6 w-6 cursor-pointer ${
                         isSignedIn && 'group-hover:block'
@@ -154,11 +137,11 @@ export default function EpisodeEntry({ data }) {
               <p
                 className="ml-24 mt-3 text-lg font-medium leading-8 text-slate-700"
                 ref={refDescription}
-                onBlur={handleEditDescripcionBlur}
+                onBlur={() => handlerBlur(refDescription)}
               >
                 {data.description}
               </p>
-              <button onClick={handleEditDescripcion}>
+              <button onClick={() => handleEdit(refDescription)}>
                 <Edit
                   className={`absolute -right-4 -top-4 hidden h-6 w-6 cursor-pointer ${
                     isSignedIn && 'group-hover:block'
@@ -168,10 +151,6 @@ export default function EpisodeEntry({ data }) {
             </div>
           </header>
           <hr className="my-12 border-gray-200" />
-          {/* <div
-            className="prose prose-slate mt-14 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-900 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2:nth-of-type(3n)]:before:bg-violet-200"
-            dangerouslySetInnerHTML={{ __html: episode.content }}
-          /> */}
           <div className="prose prose-slate mt-14 [&>div>h2]:mt-12 [&>div>h2]:flex [&>div>h2]:items-center [&>div>h2]:font-mono [&>div>h2]:text-sm [&>div>h2]:font-medium [&>div>h2]:leading-7 [&>div>h2]:text-slate-900 [&>div>h2]:before:mr-3 [&>div>h2]:before:h-3 [&>div>h2]:before:w-1.5 [&>div>h2]:before:rounded-r-full [&>div>h2]:before:bg-primary [&>div>ul]:mt-6 [&>div>ul]:list-['\2013\20'] [&>div>ul]:pl-5 [&>div>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>div>h2:nth-of-type(3n)]:before:bg-violet-200">
             <div className="">
               <h2 id="topics" className="">
@@ -179,25 +158,12 @@ export default function EpisodeEntry({ data }) {
               </h2>
             </div>
             <div className="group relative">
-              <ul ref={refTopiscs} onBlur={handleEditTopicsBlur}>
-                <li>
-                  What are TPS reports exactly, and what’s the motivation for
-                  adding the cover sheet?
-                </li>
-                <li>
-                  How Bill preserves such a cool and calm demeanor, despite the
-                  extreme consequences Initech faces for not finishing their Y2K
-                  upgrades on time, and why it’s important to protect the staff
-                  from that stress
-                </li>
-                <li>
-                  Why suspenders and belts aren’t enough on their own, and
-                  should be used together
-                </li>
-                <li>The backstory behind how Bill purchased his Porsche 911</li>
-                <li>The real reason he needed the red stapler for himself</li>
-              </ul>
-              <button onClick={handleEditTopics}>
+              <ul
+                ref={refTopiscs}
+                onBlur={() => handlerBlur(refTopiscs)}
+                dangerouslySetInnerHTML={{ __html: sanitizeHtml(data.topics) }}
+              />
+              <button onClick={() => handleEdit(refTopiscs)}>
                 <Edit
                   className={`absolute -right-4 -top-4 hidden h-6 w-6 cursor-pointer ${
                     isSignedIn && 'group-hover:block'
